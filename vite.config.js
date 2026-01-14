e45e99bfdb1b1862a91823311f77de089b5e24a9
yashamitp <yashprajapati.9769@gmail.com>
2026-01-14 14:23:53 +0530

    Fix base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/" : "/",
   plugins: [
     react(),
     tailwindcss(),
@@ -10,6 +11,6 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-     allowedHosts: ['07ea39da0efe.ngrok-free.app']
-  }
-})
+    allowedHosts: ['07ea39da0efe.ngrok-free.app'],
+  },
+}))
